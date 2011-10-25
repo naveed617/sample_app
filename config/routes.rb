cd76590ffb77db6233602c528005b795db0c043e
Naveed Ahmad <naveed@iris-ink.com>
2011-10-24 17:55:47 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 SampleApp::Application.routes.draw do
   
-  match '/contact', to: => 'pages#contact'
-  match '/about', to: => 'pages#about'
-  match '/help', to: => 'pages#help'
+  
+  
+  
+  get "users/new"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'pages#signup'
+  
   
   root :to => 'pages#home'
+  
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
